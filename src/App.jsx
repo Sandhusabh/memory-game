46ffaf56174a7460a97cdd2bf033df9cde0b46f8
Raman Sandhu <raman64854@gmail.com>
2025-04-09 23:53:53 -0700

    added images

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Layout from './components/Layout.jsx'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ImageCard from "./components/Card.jsx";
 
 function App() {
-  const [currentScore, setCurrentScore] = useState(0)
-  const [highScore, setHighScore] = useState(0)
+  const [currentScore, setCurrentScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [clicked, setClicked] = useState([]);
+  const [gameOver, setGameOver] = useState(false);
+  const [numberOfCards, setNumberOfCards] = useState(4);
 
+  const handleCardClick = (key) => {
+    console.log(key);
+    if (clicked.includes(key)) {
+      console.log("Game Over");
+    } else {
+      setClicked([...clicked, key]);
+    }
+  };
+
+  const getCards = () => {
+    const cards = [];
+    for (let i = 0; i < numberOfCards; i++) {
+      let element = (
+        <div onClick={(e) => handleCardClick(i)} key={i}>
+          <ImageCard key={i} />
+        </div>
+      );
+      cards.push(element);
+    }
+    return cards;
+  };
   return (
     <>
-        <Layout/>
+      <div className="App">{getCards()}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Body.jsx
+++
@@ -1,9 +0,0 @@
-import Card from "./Card.jsx";
-export default function Body() {
-  return (
-    <div className="body">
-      this is the body
-      <Card />
-    </div>
-  );
-}
